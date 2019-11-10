c1f5812ab05ee50860be2a46f73d11ee709fd87e
Rafael Ahrons <rafael.ahrons@gmail.com>
2019-11-10 15:12:44 -0300

    Add movement

---
+++ EndlessRunnerYoutube/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float Yincrement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement); // aumentar a posição no eixo Y pq queremos que ele se movimente para cima
+            transform.position = targetPos;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement); // diminuir a posição no eixo y pq queremos que ele se movimente para baixo
+            transform.position = targetPos;
+        }
+
+        
+    }
+}
